d7373fbba4f93c28a8d2ab77440e2e7f5ea91e56
rboj982 <r.boj982@gmail.com>
2023-09-22 22:23:40 +0200

    cars offer section

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("hello");
+// JavaScript za kontrolu galerije slika
+
+const images = document.querySelectorAll('.cars-offer-one img');
+const prevButton = document.querySelector('.prev-slide');
+const nextButton = document.querySelector('.next-slide');
+let currentImage = 0;
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    if (i === index) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+prevButton.addEventListener('click', () => {
+  currentImage = (currentImage - 1 + images.length) % images.length;
+  showImage(currentImage);
+});
+
+nextButton.addEventListener('click', () => {
+  currentImage = (currentImage + 1) % images.length;
+  showImage(currentImage);
+});
+
+// Prikaz prve slike kada se stranica učita
+showImage(currentImage);
