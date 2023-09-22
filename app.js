c292fa2a97925a0cac53a43624ea3b62c5c54713
rboj982 <r.boj982@gmail.com>
2023-09-22 22:54:18 +0200

    cars offer section in progres

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-// JavaScript za kontrolu galerije slika
-
 const images = document.querySelectorAll('.cars-offer-one img');
-const prevButton = document.querySelector('.prev-slide');
-const nextButton = document.querySelector('.next-slide');
-let currentImage = 0;
+const modal = document.createElement('div');
+modal.className = 'modal';
+document.body.appendChild(modal);
 
-function showImage(index) {
-  images.forEach((image, i) => {
-    if (i === index) {
-      image.classList.add('active');
-    } else {
-      image.classList.remove('active');
-    }
+images.forEach((image, i) => {
+  image.addEventListener('click', () => {
+    modal.innerHTML = `
+      <div class="modal-content">
+        <span class="prev-slide">&lt;</span>
+        <img src="${image.src}" alt="">
+        <span class="next-slide">&gt;</span>
+      </div>
+    `;
+
+    const prevButton = modal.querySelector('.prev-slide');
+    const nextButton = modal.querySelector('.next-slide');
+    let currentIndex = i;
+
+    prevButton.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      modal.querySelector('img').src = images[currentIndex].src;
+    });
+
+    nextButton.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % images.length;
+      modal.querySelector('img').src = images[currentIndex].src;
+    });
+
+    modal.style.display = 'block';
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
   });
-}
-
-prevButton.addEventListener('click', () => {
-  currentImage = (currentImage - 1 + images.length) % images.length;
-  showImage(currentImage);
 });
-
-nextButton.addEventListener('click', () => {
-  currentImage = (currentImage + 1) % images.length;
-  showImage(currentImage);
-});
-
-// Prikaz prve slike kada se stranica učita
-showImage(currentImage);
