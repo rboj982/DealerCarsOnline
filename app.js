ab6fec15f6c75803add26482b07dd71d80509ff9
rboj982 <r.boj982@gmail.com>
2023-09-23 19:48:12 +0200

    work on offer JS

---
+++ app.js
@@ -0,0 +1,6 @@
+// Funkcija za otvaranje popup prozora
+function openPopup() {
+    popup.style.display = 'block';
+    displayImage(currentImageIndex);
+    console.log('popup hello'); // Dodajte ovu liniju za ispis u konzolu
+}
